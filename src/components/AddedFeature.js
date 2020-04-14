c50db0cf270c4eaa1389f750839d0d840215f366
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-04-14 17:46:18 -0500

    adding code

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import { removeFeature } from '../actions/featuresActions';
+import { connect } from 'react-redux';
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className="button"
+        onClick={() =>
+          props.removeFeature({ id: props.id, price: props.feature.price })
+        }
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { removeFeature }
+)(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import { addFeature } from '../actions/featuresActions';
+import { connect } from 'react-redux';
 const AdditionalFeature = props => {
+  const handleClick = e =>{
+    // e.preventDefault();
+    props.addFeature(e);
+
+  }
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"
+      onClick = {()=>handleClick(props.feature.id)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+const mapStateToProps = state => {
+  return {
+  }
+}
 
-export default AdditionalFeature;
+export default connect(
+  mapStateToProps,
+  { addFeature }
+)(AdditionalFeature);
